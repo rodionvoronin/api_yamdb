905eca14572a4d49320e99a18d8b87d5529dcd05
Родион Воронин <vroooom10@yandex.ru>
2023-02-01 02:59:10 +0500

    title field added

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -5,6 +5,10 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 User = get_user_model()
 
 
+class Title(models.Model):
+    pass
+
+
 class Review(models.Model):
     text = models.TextField()
     score = models.PositiveSmallIntegerField(
@@ -14,7 +18,10 @@ class Review(models.Model):
         'Дата публикации', auto_now_add=True
     )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts'
+        User, on_delete=models.CASCADE, related_name='reviews'
+    )
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='reviews'
     )
 
     def __str__(self):
