c63bd822e39a509edafe80bd2e54a1c0d2f3a560
Родион Воронин <vroooom10@yandex.ru>
2023-02-01 02:15:43 +0500

    Review model created

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+User = get_user_model()
+
+
+class Review(models.Model):
+    text = models.TextField()
+    score = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='posts'
+    )
+
+    def __str__(self):
+        return self.text
