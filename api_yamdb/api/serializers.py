ac0794ee1639d9d3ea2646b8cff0b157acbffa07
Родион Воронин <vroooom10@yandex.ru>
2023-02-02 08:56:49 +0500

    views and serializers changed

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import get_user_model
 
 from rest_framework.serializers import (
-    CurrentUserDefault, ModelSerializer, SlugRelatedField, ValidationError
+    CurrentUserDefault, ModelSerializer, SlugRelatedField, ValidationError,
+    PrimaryKeyRelatedField,
 )
 # from rest_framework.validators import UniqueTogetherValidator
 
-from reviews.models import Title, Review
+from reviews.models import Title, Review, Comment
 
 User = get_user_model()
 
@@ -19,6 +20,31 @@ class TitleSerializer(ModelSerializer):
 
 
 class ReviewSerializer(ModelSerializer):
+    author = SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+    )
+    title = SlugRelatedField(
+        slug_field='id',
+        queryset=Title.objects.all(),
+    )
+
     class Meta:
         fields = '__all__'
         model = Review
+        read_only_fields = ('title', 'author')
+
+
+class CommentSerializer(ModelSerializer):
+    author = SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+    )
+    review = PrimaryKeyRelatedField(
+        read_only=True,
+    )
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ('review', 'author')
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,4 @@
-# from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404
 # from rest_framework.filters import SearchFilter
 # from rest_framework.mixins import CreateModelMixin, ListModelMixin
 # from rest_framework.pagination import LimitOffsetPagination
@@ -8,7 +8,7 @@ from rest_framework.viewsets import (
 )
 
 # from api.permissions import OwnerOrReadOnly
-from api.serializers import TitleSerializer, ReviewSerializer
+from api.serializers import TitleSerializer, ReviewSerializer, CommentSerializer
 
 from reviews.models import Title, Review
 
@@ -24,5 +24,34 @@ class TitleViewSet(ModelViewSet):
 
 
 class ReviewViewSet(ModelViewSet):
-    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+#    permission_classes = (OwnerOrReadOnly,)
+
+    def get_title(self):
+        title_id = self.kwargs.get('title_id')
+        return get_object_or_404(Title, pk=title_id)
+
+    def get_queryset(self):
+        title = self.get_title()
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title = self.get_title()
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(ModelViewSet):
+    serializer_class = CommentSerializer
+#    permission_classes = (OwnerOrReadOnly,)
+
+    def get_review(self):
+        review_id = self.kwargs.get('review_id')
+        return get_object_or_404(Review, pk=review_id)
+
+    def get_queryset(self):
+        review = self.get_review()
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review = self.get_review()
+        serializer.save(author=self.request.user, review=review)
