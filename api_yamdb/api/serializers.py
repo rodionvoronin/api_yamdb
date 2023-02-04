b108ef269b4eaf31db254c1a40471a798932e93f
Родион Воронин <vroooom10@yandex.ru>
2023-02-05 00:45:46 +0300

    serializer completed

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,12 +1,15 @@
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import ValidationError
 
 from rest_framework.serializers import (
     CurrentUserDefault, ModelSerializer, SlugRelatedField, ValidationError,
     PrimaryKeyRelatedField, IntegerField,
 )
+from django.core.validators import MaxValueValidator, MinValueValidator
 # from rest_framework.validators import UniqueTogetherValidator
 
-from reviews.models import Title, Review, Comment
+from reviews.models import Title, Review
 
 User = get_user_model()
 
@@ -24,20 +27,24 @@ class ReviewSerializer(ModelSerializer):
         read_only=True,
         slug_field='username',
     )
-    title = SlugRelatedField(
-        slug_field='id',
-        queryset=Title.objects.all(),
-    )
+    title_id = self.context['view'].kwargs.get('title_id')
+    title = get_object_or_404(Title, pk=title_id)
     score = IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(10)],
         required=True,
     )
 
     def validate(self, data):
-        if self.context['request'].user == data['reviews']:
-            raise serializers.ValidationError(
-                'Вы уже писали отзыв на это произведение'
-            )
+        request = self.context['request']
+        author = request.user
+        title_id = self.context['view'].kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if request.method == 'POST':
+            if title.reviews.select_related('title').filter(author=author):
+                raise ValidationError(
+                    'Отзыв уже существует'
+                )
+
         return data
 
     class Meta:
