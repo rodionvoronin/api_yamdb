836c221edc75c65cad1b09b0af1cf4a3b6685f0e
Alexandr Koshkin <ajkoshkin@yandex.ru>
2023-02-09 22:01:25 +0300

    Add CSV import for User model v.004

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 
 from .models import User
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-
-@admin.register(User)
+"""
+#@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
+        'id',
         'username',
         'email',
         'role',
@@ -16,4 +19,21 @@ class UserAdmin(admin.ModelAdmin):
     )
     search_fields = ('username', 'role',)
     list_filter = ('username',)
-    empty_value_display = '--пусто--'
+    empty_value_display = '-пусто-'
+"""
+
+class UserResource(resources.ModelResource):
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'role',
+                  'bio', 'first_name', 'last_name')
+
+
+# вывод данных на странице
+class ImportAdmin(ImportExportModelAdmin):
+    resource_classes = [UserResource]
+
+
+#admin.site.register(User, UserAdmin)
+admin.site.register(User, ImportAdmin)
